43b36f83a9e7b8ad72e995eb79e0dc27acdadd2e
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-23 12:11:51 -0600

    create state and bound it to the input

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,37 @@ import React, { Component } from "react";
 import Input from "./Input";
 
 class Card extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      color: "Pink",
+      pluralNoun: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({ color: event.target.value });
+  }
+
   render() {
     return (
+      <div>
+        <h1>{this.state.color}</h1>
+        <input
+          type="text"
+          onChange={(event) => this.handleInputChange(event)}
+        />
+      </div>
+    );
+
+    return (
       <div className="card">
-        <Input title="Hi" />
+        <h1>{this.state.color}</h1>
+        <Input title="Color" />
+        <Input title="Component" />
       </div>
     );
   }
