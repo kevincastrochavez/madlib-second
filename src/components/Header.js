d1813621c8bd1739070dfe96b62a70fa6ef17925
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-24 12:11:02 -0600

    styled the skew in the background, the card component, and started tu style the inputs

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
+      <div className="header__skew"></div>
+      <div className="header__border"></div>
       <h1>Bottega Mad Libs</h1>
       <p>
         Fill out the fields below anc click the generate button to see the Mad
